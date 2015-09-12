2cf387ddb94c093e0330f47fe20392ce1922b126
Daniel Adelantado <dadelantado@gmail.com>
2015-09-12 11:59:34 -0500

    Update for new YouTube API

--- pull.py
+++ pull.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-
 import urllib, json, subprocess, time, sys, getopt
-#print "Channel?",
-#author = str(sys.argv[1])
-#author = 'dadelantado'
 try:
     options, remainder = getopt.getopt(sys.argv[1:], 'u:i:h', ['user=', 'id=', 'help'])
 except getopt.GetoptError as err:
     print(err) 
-    print 'Use -i for id or -u for username' # will print something like "option -a not recognized"
-    
-        #usage()
-        sys.exit(2)
-print options
+    print 'Use -i for id or -u for username'
+    sys.exit(2)
+
 for opt, arg in options:
     if opt in ('-u', '--user'):
         types = 'forUsername'
@@ -27,33 +19,24 @@ for opt, arg in options:
         print 'Use -i for id or -u for username'
         sys.exit(2)
             
-
-foundAll = False
-ind = 0
-#videos = []
 apikey = 'AIzaSyA7s-mBPBU5snEKPZ7CAuLwIuvGa6hRGyc'
+ind = 0
 
 #1st Part: retreive uploads id, forUsername(with username) or id (channel id)
-
 inp = urllib.urlopen(r'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&{0}={1}&key={2}'.format( types, author, apikey ) )
 resp = json.load(inp)
 inp.close()
-print "Generating playlist..."
+print "Generating playlist ..."
 uploadsid = resp['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-#print resp
-#print uploadsid
 
 #2nd Part: retreive list of videos
 inp = urllib.urlopen(r'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={0}&key={1}&maxResults=50'.format(uploadsid, apikey))
 videolistresp = json.load(inp)
 inp.close()
 videolist = videolistresp['items']
-#for videos in videolist:
- #print videolist[ind]['snippet']['title'],
- #print videolist[ind]['snippet']['resourceId']['videoId']
 firstvideo = videolist[ind]['snippet']['resourceId']['videoId']
- #ind += 1
 
+#3rd Part: Create HTML file
 file = open(author+'.html', 'w')
 file.write('<div class="responsive-video-list"><div class="featured-video"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+firstvideo+'?autoplay=0&amp;rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autohide=1" frameborder="0" allowfullscreen id="FeaturedVideoID"></iframe></div><ul>')
 ind = 0
@@ -65,6 +48,9 @@ for videos in videolist:
  ind += 1
 
 file.write('</ul></div>')
+
+#4th Part: Open HTML file with BBEdit
+print "Opening HMTL file ..."
 time.sleep(3)
 cmd = 'bbedit '+author+'.html'
 procc = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
